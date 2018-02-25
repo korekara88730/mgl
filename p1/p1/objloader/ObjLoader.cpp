9876a57c36040601e5d293585c46daddee2ac0b8
korekara88730 <502765299@qq.com>
2018-02-25 13:59:19 +0800

    +add load json mode data to ObjModel feature

--- p1/p1/objloader/ObjLoader.cpp
+++ p1/p1/objloader/ObjLoader.cpp
@@ -13,6 +13,59 @@
 #include <iostream>
 #include <sstream>
 
+
+static const int FACE_VERTICE_CNT = 3;
+
+ObjModel::ObjModel()
+:_verticePosData(nullptr)
+,_verticeUVData(nullptr)
+,_verticeNormalData(nullptr)
+//,_vao(0)
+,_vboPos(0)
+//,_vboUV(0)
+//,_vboNormal(0)
+,_faceCnt(0)
+{
+    
+}
+
+ObjModel::~ObjModel()
+{
+    
+}
+
+void ObjModel::initWithJson(cJSON* pJson)
+{
+    cJSON* faces = cJSON_GetObjectItem(pJson,"faces");
+    int faceCnt = cJSON_GetArraySize(faces);
+    
+    _faceCnt = faceCnt;
+    
+    int posDataLen = faceCnt * FACE_VERTICE_CNT;
+    _verticePosData = new float[posDataLen];
+    for(int index = 0;index < faceCnt;index++)
+    {
+        cJSON* faceData = cJSON_GetArrayItem(faces,index);
+        for(int vIndex = 0;vIndex < FACE_VERTICE_CNT;vIndex++)
+        {
+            cJSON* vertexPosData = cJSON_GetObjectItem(cJSON_GetArrayItem(faceData,vIndex),"vertexIndex");
+            int vertexIndex = vertexPosData->valueint;
+            cJSON* allVertice = cJSON_GetObjectItem(pJson,"vertice");
+            float x = (float)cJSON_GetObjectItem(cJSON_GetArrayItem(allVertice,vertexIndex - 1),"x")->valuedouble;
+            float y = (float)cJSON_GetObjectItem(cJSON_GetArrayItem(allVertice,vertexIndex - 1),"y")->valuedouble;
+            float z = (float)cJSON_GetObjectItem(cJSON_GetArrayItem(allVertice,vertexIndex - 1),"z")->valuedouble;
+            _verticePosData[index * FACE_VERTICE_CNT + vIndex] = x;
+            _verticePosData[index * FACE_VERTICE_CNT + vIndex] = y;
+            _verticePosData[index * FACE_VERTICE_CNT + vIndex] = z;
+        }
+    }
+    // gen pos vbo
+    glGenBuffers(1,&_vboPos);
+    glBindBuffer(GL_ARRAY_BUFFER,_vboPos);
+    glBufferData(GL_ARRAY_BUFFER,sizeof(_verticePosData),_verticePosData,GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER,0);
+}
+
 ObjModel* loadObj(std::string path)
 {
     std::string strJson;
@@ -35,10 +88,20 @@ ObjModel* loadObj(std::string path)
     {
         printf("Can not parse JSON object.");
     }
+    // test json case
+    /*
+    cJSON* vertice = cJSON_GetObjectItem(pJson,"vertice");
+    int verticeCnt = cJSON_GetArraySize(vertice);
+
+    cJSON* item = cJSON_GetArrayItem(vertice,1);
     
+    float val = item->valuedouble;
+    
+    cJSON* test = cJSON_GetObjectItem(pJson,"xxx");
+    */
     
     ObjModel* pObj = new ObjModel();
-    printf("%s",path.c_str());
+    pObj->initWithJson(pJson);
     return pObj;
 }
 
--- p1/p1/objloader/ObjLoader.hpp
+++ p1/p1/objloader/ObjLoader.hpp
@@ -4,10 +4,32 @@
 
 #include <stdio.h>
 #include <iostream>
+#include "cJSON.h"
+#include <GL/glew.h>
 
 class ObjModel
 {
+public:
+    ObjModel();
+    virtual ~ObjModel();
+    void initWithJson(cJSON* pJson);
     
+    GLuint getPosVBO(){ return _vboPos;}
+    int getFaceCnt(){ return _faceCnt;}
+    
+private:
+    float* _verticePosData;
+    float* _verticeUVData;
+    float* _verticeNormalData;
+    
+private:
+//    GLuint  _vao;
+    GLuint  _vboPos;
+//    GLuint  _vboUV;
+//    GLuint  _vboNormal;
+    
+    
+    int _faceCnt;
     
 };
 
